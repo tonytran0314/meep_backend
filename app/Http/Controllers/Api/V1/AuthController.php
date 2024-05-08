a320d3eebefd849d752ff2b4fbb74facc6e89831
Gia Huy Tran <tonytran0314@gmail.com>
2024-05-08 14:44:30 -0400

    feature signup

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,19 +3,34 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(Request $param) {
+    public function login(Request $request) {
         return response()->json('login');
     }
 
-    public function signup() {
-        return 'signup';
+    public function signup(StoreUserRequest $request) {
+        
+        $request->validated($request->all());
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of ' . $user->name)->plainTextToken
+        ]);
     }
 
     public function logout() {

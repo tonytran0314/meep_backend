d47d0068cfb40fc81ec73a012ec7d0b8115d24a1
Gia Huy Tran <tonytran0314@gmail.com>
2024-05-08 15:28:26 -0400

    feature login

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(Request $request) {
-        return response()->json('login');
+    public function login(LoginRequest $request) {
+        
+        $request->validated($request->all());
+
+        $user = User::where('email', $request->email)->first();
+
+        if(! $user || ! Hash::check($request->password, $user->password)) {
+            return $this->error('', 'Credentials do not match', 401);
+        }
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of ' . $user->name)->plainTextToken
+        ]);
     }
 
     public function signup(StoreUserRequest $request) {

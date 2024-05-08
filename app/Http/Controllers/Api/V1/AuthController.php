33aaaba8939e0268fa339b623143f3dd9783f883
Gia Huy Tran <tonytran0314@gmail.com>
2024-05-08 13:58:22 -0400

    setup routes

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    use HttpResponses;
+
+    public function login(Request $param) {
+        return response()->json('login');
+    }
+
+    public function signup() {
+        return 'signup';
+    }
+
+    public function logout() {
+        return 'logout';
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function() {
-    Route::get('/hello', function () {
-        return 'This is Laravel API';
-    });
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
